208cd663e4a42b87981d6720b21e2bcd0d6797de
Parth Nagar <rakeshnagar1301@gmail.com>
2025-06-12 17:14:39 +0530

    "Day 5 code + project"

---
+++ DAY5/Day5_2_range.py
@@ -0,0 +1,11 @@
+#he combination of the range() function with the Python For Loop allows us to run a loop for as many times as we wish. Instead of looping through each item in a List, we can loop through a range of numbers.
+
+#Range Function
+#range(1, 10)
+
+#This code doesn't do anything by itself. For example, if you tried to print it, it would not give you individual numbers.
+
+#But it can be used in conjunction with For Loops. e.g.
+
+for number in range(1,10):
+    print(number)
